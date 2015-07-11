6a89631a6669acfab49a4e2aa4a127fc2d929968
JARED ARKIN <ARKINJB@GMAIL.COM>
2015-07-11 15:48:10 -0400

    initial commit. added pseudo code notes

---
+++ script.js
@@ -0,0 +1,7 @@
+//when winow loads, display message "please select options and click new game to start"
+//when new game is clicked:
+  //message is hidden
+  //user is prompted for name
+    //name is stored as variable
+    //name is displayed on scoreboard
+    //timer is started if used
