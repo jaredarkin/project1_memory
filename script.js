7f9a22bd04e272e275ffa46ba3fe6a7cc54e4edc
JARED ARKIN <ARKINJB@GMAIL.COM>
2015-07-11 18:10:52 -0400

    added functionality for card clicks and comparison

--- script.js
+++ script.js
@@ -1,15 +1,19 @@
 //when window loads, display message in header "please select options and click new game to start"
 //change background image of cards depending on option clicked - done
   //only one option can be checked at once - radio buttons
+var deckImage;
 $("input[name='deckDesign']").change(function(){
   if ($(this).val() === "cats"){
     $("p.card").css("background-image", "url(images/spacecat.jpg)");
+    deckImage = images/spacecat.jpg;
   }
   if ($(this).val() === "logo"){
     $("p.card").css("background-image", "url(images/ga_logo.png)");
+    deckImage = images/ga_logo.png;
   }
   if ($(this).val() === "bob"){
     $("p.card").css("background-image", "url(images/gene2.gif)");
+    deckImage = images/gene2.gif;
   }
 });
 //change number of cards (<p>) depending on option clicked
@@ -29,15 +33,31 @@ $("#newGame").on("click", function(){
 });
 
 var clickCounter = 0;
+var guessCounter = 0;
+var firstCard;
+var firstCardValue;
+var secondCard;
+var secondCardValue;
 //when a card is clicked
-  //click counter increases to 1
-  //background image on card is removed
-  //centered text is shown
-//when a second card is clicked
-  //click counter increases to 2
-  //background image is removed
-  //centered text is shown
-  //guesses counter is increased
+$("p.card").on("click", function(){
+  if(clickCounter === 0){
+    clickCounter++;
+    console.log(clickCounter);
+    $(this).addClass("flipped");
+    firstCard = $(this);
+    firstCardValue = $(this).text();
+    console.log(firstCardValue);
+  } else {
+    clickCounter = 0;
+    console.log(clickCounter);
+    $(this).addClass("flipped");
+    secondCard = $(this);
+    secondCardValue = $(this).text();
+    console.log(secondCardValue);
+    compareCards();
+  }
+});
+
 //if text matches on both cards
   //cards remain as they are
   //click counter resets to 0
@@ -46,3 +66,25 @@ var clickCounter = 0;
   //on both cards the text is hidden
   //on both cards the image is shown
   //click counter resets to 0
+function compareCards(){
+  guessCounter++;
+  if (firstCardValue === secondCardValue){
+    console.log("youve got a match");
+  } else {
+    console.log("no match");
+    setTimeout(
+            function() {
+                firstCard.removeClass("flipped");
+                secondCard.removeClass("flipped");
+            },
+            2000);
+  }
+}
+  //click counter increases to 1
+  //background image on card is removed
+  //centered text is shown
+//when a second card is clicked
+  //click counter increases to 2
+  //background image is removed
+  //centered text is shown
+  //guesses counter is increased
